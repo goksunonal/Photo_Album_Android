08cdb46fc8a1047a400c17a8d0201413caa2d89f
goksunonal <32015340+goksunonal@users.noreply.github.com>
2018-11-04 12:02:15 +0300

    Initializing Android App

---
+++ src/main/java/eniac/photo_album_android/PhotoFields.java
@@ -0,0 +1,9 @@
+package eniac.photo_album_android;
+
+import android.widget.ImageView;
+
+public class PhotoFields {
+        String album;
+        ImageView photo;
+        String uploaded_at;
+}
---
+++ src/main/java/eniac/photo_album_android/PhotoResponse.java
@@ -0,0 +1,42 @@
+package eniac.photo_album_android;
+
+import java.util.Arrays;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class PhotoResponse {
+    private static final String API_BASE_URL = "https://photo-album-eniac.herokuapp.com/";
+    private static List<PhotoFields> Photos;
+    private void initiatePhotoApi(String location, String r_type) {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(API_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        PhotoREST service = retrofit.create(PhotoREST.class);
+        Call<PhotoFields[]> call = service.getPhotos();
+        call.enqueue(new Callback<PhotoFields[]>() {
+                         @Override
+                         public void onResponse(Call<PhotoFields[]> call, Response<PhotoFields[]> response) {
+                             if(response.isSuccessful()) {
+                                 Photos = Arrays.asList(response.body());
+                             } else {
+                                 System.out.println(response.errorBody());
+                             }
+                         }
+
+                         @Override
+                         public void onFailure(Call<PhotoFields[]> call, Throwable t) {
+
+                         }
+                     }
+
+        );
+    }
+}
